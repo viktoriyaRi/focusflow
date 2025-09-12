96a5379aed12ca578366a01299b53ee4902a56d9
viktoriyaRi <vikamalik30@gmail.com>
2025-09-12 09:52:17 -0400

    build: add vite legacy plugin; fix: notification icons for GH Pages; mobile error reporter

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "iOS >= 12", "Android >= 7", "not IE 11"],
+      modernPolyfills: true,
+    }),
+  ],
   base: "/focusflow/",
 });
